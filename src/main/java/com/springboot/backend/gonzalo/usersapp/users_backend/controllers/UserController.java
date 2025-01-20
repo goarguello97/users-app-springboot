6fed768ae046bb9bd49cd66868688f1055697c74
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-01-20 18:12:45 -0300

    first commit

---
+++ src/main/java/com/springboot/backend/gonzalo/usersapp/users_backend/controllers/UserController.java
@@ -0,0 +1,109 @@
+package com.springboot.backend.gonzalo.usersapp.users_backend.controllers;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.backend.gonzalo.usersapp.users_backend.entities.User;
+import com.springboot.backend.gonzalo.usersapp.users_backend.services.UserService;
+
+import jakarta.validation.Valid;
+
+@CrossOrigin(origins = { "http://localhost:4200/" })
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<User> list() {
+        return this.service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> show(@PathVariable Long id) {
+
+        Optional<User> optionalUser = this.service.findById(id);
+
+        if (optionalUser.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(optionalUser.orElseThrow());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Collections.singletonMap("error", "Usuario no existe."));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return validations(result);
+
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@Valid @RequestBody User user, BindingResult result, @PathVariable Long id) {
+
+        if (result.hasErrors()) {
+            return validations(result);
+
+        }
+
+        Optional<User> optionalUser = this.service.findById(id);
+
+        if (optionalUser.isPresent()) {
+            User userDb = optionalUser.get();
+            userDb.setEmail(user.getEmail());
+            userDb.setLastname(user.getLastname());
+            userDb.setName(user.getName());
+            userDb.setPassword(user.getPassword());
+            userDb.setUsername(user.getUsername());
+            return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(userDb));
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Optional<User> optionalUser = this.service.findById(id);
+
+        if (optionalUser.isPresent()) {
+            this.service.deleteById(id);
+
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+    }
+
+    private ResponseEntity<?> validations(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+        result.getFieldErrors().forEach(error -> {
+            errors.put(error.getField(), "El campo " + error.getField() + " " + error.getDefaultMessage());
+        });
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+}
