fd2762d6404ac68eb788f5abd8f1c509468cd3d5
Gonzalo Argüello <arguellogonzalo97@gmail.com>
2025-01-20 19:30:34 -0300

    add pagination

--- src/main/java/com/springboot/backend/gonzalo/usersapp/users_backend/services/UserService.java
+++ src/main/java/com/springboot/backend/gonzalo/usersapp/users_backend/services/UserService.java
@@ -3,6 +3,8 @@ package com.springboot.backend.gonzalo.usersapp.users_backend.services;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.lang.NonNull;
 
 import com.springboot.backend.gonzalo.usersapp.users_backend.entities.User;
@@ -10,6 +12,8 @@ import com.springboot.backend.gonzalo.usersapp.users_backend.entities.User;
 public interface UserService {
 
     List<User> findAll();
+
+    Page<User> findAll(Pageable pageable);
 
     Optional<User> findById(@NonNull Long id);
 
